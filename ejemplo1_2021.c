46f46ea945db0c75fc53b83a6563d46377786eeb
Claudio Henríquez <chenriquezb@ucentral.cl>
2021-09-01 18:52:10 -0400

    test 1

---
+++ ejemplo1_2021.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(){
+	//sección de declaraciones
+	int num;
+	int a;
+	char *b;
+
+
+	//sección de instrucciones
+	printf("Ingrese un numero\n"); //System.out.println();
+	scanf("%i",&num);
+	printf("El numero ingresado es: %i\n",num );
+
+	printf("Ingrese un caracter\n");
+	scanf("%s",b);
+	printf("El caracter ingresado es: %s\n",b );
+
+
+
+
+}
